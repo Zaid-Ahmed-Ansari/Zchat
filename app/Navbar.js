a6c9bd9d95de1eeb40198993bf4500479889c3ab
Zaid <ahmedzaid2627@gmail.com>
2025-01-14 23:05:03 +0530

    Done 3

--- app/Navbar.js
+++ app/Navbar.js
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from "react";
+// import { useState } from "react";
 import Link from "next/link";
 import './globals.css'
 import { AiOutlineWechatWork } from "react-icons/ai";
--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 // import Image from "next/image";
-import { Metadata } from 'next'
+// import { Metadata } from 'next'
 import Link from 'next/link';
  
 export const metadata = {
